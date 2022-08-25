24611b543584fa7a5b67a113754dd51ca564d246
Hokare <62426977+Hokare@users.noreply.github.com>
2022-08-26 00:58:58 +0200

    .

---
+++ bin/start.bat
@@ -0,0 +1,2 @@
+cd ../
+start bin\start-dev.bat
--- src/main.py
+++ src/main.py
@@ -1,8 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def index():
+def accueil():
     return render_template("accueil.jinja")
+
+
+@app.route("/competences")
+def competences():
+    return render_template("competences.jinja")
+
+
+@app.route("/diplomes")
+def diplomes():
+    return render_template("diplomes.jinja")
+
+
+@app.route("/experiences")
+def experiences():
+    return render_template("experiences.jinja")
+
+
+@app.route("/Martin-Barbey")
+def MartinBarbey():
+    return send_file("static/document/Martin-Barbey.pdf")
+
+
+@app.route("/loisirs")
+def loisirs():
+    return render_template("loisirs.jinja")
+
+
+@app.route("/projets")
+def projets():
+    return render_template("projets.jinja")
+
+
+@app.route("/scolarite")
+def scolarite():
+    return render_template("scolarite.jinja")
